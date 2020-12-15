9f4a016db0ee29013d417efaea99620b48e99205
recolte00 <yukiyako2.lulu1920@gmail.com>
2020-12-16 01:01:09 +0900

    [Create] UserList Display

--- SpringSample/src/main/java/com/example/demo/login/controller/SignupController.java
+++ SpringSample/src/main/java/com/example/demo/login/controller/SignupController.java
@@ -3,6 +3,7 @@ package com.example.demo.login.controller;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,10 +12,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.login.domain.model.GroupOrder;
 import com.example.demo.login.domain.model.SignupForm;
+import com.example.demo.login.domain.model.User;
+import com.example.demo.login.domain.repository.jdbc.service.UserService;
 
 @Controller
 public class SignupController {
+	
+	@Autowired
+	private UserService userService;
 	//ラジオボタンの実装
 	private Map<String, String> radioMarriage;
 	
@@ -37,13 +44,30 @@ public class SignupController {
 	}
 	
 	@PostMapping("/signup")
-	public String postSignUp(@ModelAttribute @Validated SignupForm form, BindingResult bindingResult, Model model) {
+	public String postSignUp(@ModelAttribute @Validated(GroupOrder.class) SignupForm form, BindingResult bindingResult, Model model) {
 		if(bindingResult.hasErrors()) {
 			return getSignUp(form, model);
 		}
 		System.out.println(form);
+		
+		User user = new User();
+		user.setUserId(form.getUserId());
+		user.setPassword(form.getPassword());
+		user.setUserName(form.getUserName());
+		user.setBirthday(form.getBirthday());
+		user.setAge(form.getAge());
+		user.setMarriage(form.isMarriage());
+		user.setRole("ROLE_GENERAL");
+		
+		boolean result = userService.insert(user);
+		
+		if(result == true) {
+			System.out.println("insert 成功");
+		}else{
+			System.out.println("inser 失敗");
+		}
+
 		return "redirect:/login";
 	}
 
 }
-
