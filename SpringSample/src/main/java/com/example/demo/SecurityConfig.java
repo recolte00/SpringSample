d1ea29ee8e239663078736bc9c236a246899929f
recolte00 <yukiyako2.lulu1920@gmail.com>
2021-01-31 02:37:19 +0900

    [add] SpringSecurity Login Page

---
+++ SpringSample/src/main/java/com/example/demo/SecurityConfig.java
@@ -0,0 +1,71 @@
+package com.example.demo;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+// ポイント１：セキュリティ設定用クラス
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	// データソース
+	@Autowired
+	private DataSource dataSource;
+	// ユーザーIDとパスワードを取得するSQL文
+	private static final String USER_SQL = "SELECT"
+			+ " user_id,"
+			+ " password,"
+			+ " true"
+			+ " FROM"
+			+ " m_user"
+			+ " WHERE"
+			+ " user_id = ?";
+	// ユーザーのロールを取得するSQL文
+	private static final String ROLE_SQL = "SELECT"
+			+ " user_id,"
+			+ " role"
+			+ " FROM"
+			+ " m_user"
+			+ " WHERE"
+			+ " user_id = ?";
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		// ポイント２：静的リソースへのアクセスには、セキュリティを適用しない
+		web.ignoring().antMatchers("/webjars/**","/css/**");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// ポイント３：直リンクの禁止
+		//　ログイン不要ページの設定
+		http.authorizeRequests().antMatchers("/webjars/**").permitAll()
+			.antMatchers("/css/**").permitAll()
+			.antMatchers("/login").permitAll()
+			.antMatchers("/signup").permitAll()
+			.anyRequest().authenticated();
+		
+		http.formLogin()
+			.loginProcessingUrl("/login")
+			.loginPage("/login")
+			.failureUrl("/login")
+			.usernameParameter("userId")
+			.passwordParameter("password")
+			.defaultSuccessUrl("/home",true);
+		// CSRF対策を無効に設定（一時的）
+		http.csrf().disable();
+	}
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication()
+		.dataSource(dataSource)
+		.usersByUsernameQuery(USER_SQL)
+		.authoritiesByUsernameQuery(ROLE_SQL);
+	}
+}
